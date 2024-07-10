b95f6a7ab98ddf75fe0cf831f565e0159e4a912b
Anastasiapas <pasichnyk.nas23@gmail.com>
2024-07-09 23:46:32 -0700

    Homework #5

---
+++ jsBasics/Homework5/task1.js
@@ -0,0 +1,13 @@
+const averageGrade = 70;
+
+if (averageGrade < 60) {
+    console.log("Average grade is 'Unsatisfactory'")
+} else if (averageGrade <= 70) {
+    console.log("Average grade is 'Satisfactory'")
+} else if (averageGrade <= 80) {
+    console.log("Average grade is 'Good'")
+} else if (averageGrade <= 90) {
+    console.log("Average grade is 'Very Good'")
+} else if (averageGrade <= 100) {
+    console.log("Average grade is 'Excellent'")
+}
---
+++ jsBasics/Homework5/task3.js
@@ -0,0 +1,13 @@
+const number = 8;
+
+for (let i = 1; i < 11; i++) {
+    console.log(`${number} X ${i} = ${number * i}`)
+}
+
+console.log('');
+
+let i = 1;
+while (i < 11) {
+    console.log(`${number} X ${i} = ${number * i}`);
+    i++;
+}
